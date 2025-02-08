5e0ce6ff8b1eb87f05f1fd0c7c7865a61e70eb50
soldier855 <srisoldier1@gmail.com>
2025-02-08 18:07:28 +0530

    Update rb.py

--- rb.py
+++ rb.py
@@ -134,7 +134,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./ranbal {ip} {port} {duration} 800"
+        command = f"./known {ip} {port} {duration} 9 900"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
